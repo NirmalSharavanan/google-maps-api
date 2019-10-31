4ed6d4be3ad7a811c346efbcf991dc1a5de6394c
Nirmal Sharavanan <nirmalsharavanan@gmail.com>
2019-10-31 10:41:40 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,26 @@
+# import the library
+import googlemaps
+import pprint
+
+# Define the API Key.
+API_KEY = ''
+
+# Define the Clientgeocode_result
+gmaps = googlemaps.Client(key=API_KEY)
+
+geocode_result = gmaps.geocode(input("Enter place name:"))
+pprint.pprint(geocode_result)
+
+lat = geocode_result[0]["geometry"]["location"]["lat"]
+lng = geocode_result[0]["geometry"]["location"]["lng"]
+
+for details in geocode_result[0]["address_components"]:
+    if details['types'] == ['postal_code']:
+        print("Pin Code:" + details['long_name'])
+
+coord = "\"{0},{1}\"".format(lat,lng)
+print(coord)
+
+
+
+
